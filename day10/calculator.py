91e596dd72852dd2849c848ad7451ffcaf182008
Hailey <carly1982@naver.com>
2023-07-01 21:09:23 +0900

    Tenth commit
    add calculator_art.py (logo)
    and modify calculator.py code that chage data type int (num1 & num2) to float. And solve the error.

--- day10/calculator.py
+++ day10/calculator.py
@@ -1,4 +1,7 @@
+import calculator_art #from calculator_art import logo
 #Calculator
+
+print(calculator_art.logo)
 
 #Add
 def add(n1, n2):
@@ -24,27 +27,48 @@ operations = {
     "/"
 }
 
-num1 = int(input("Whats the first number? "))
-num2 = int(input("What's the next number? "))
+def calculator():
+    num1 = float(input("What's the first number? "))
+    for symbol in operations:
+        print(symbol)
+    should_continue = True
 
-for symbol in operations:
-    print(symbol)
+    while should_continue:
+        operation_symbol = input("Pick an operations: ")
+        num2 = float(input("What's the next number? "))
+        calculation_function = operations[operation_symbol]
+        answer = calculation_function(num1, num2)
 
-operation_symbol = input("Pick an operation from the line above: ")
-calculation_function = operations[operation_symbol] # important sentence => calculation_function = operations["*"] = multiply
-ask_continue = input("Type 'y' to continue calculating with 8, or type 'n' to exit.: ")
-answer = calculation_function(num1, num2) # add(n1, n2), divide(n1, n2)
+        print(f"{num1} {operation_symbol} {num2} = {answer}")
 
-end_Calculator = (ask_continue == "y")
-while end_Calculator:
-    num2 = int(input("What's the next number? "))
-    operation_symbol = input("Pick an operation from the line above: ")
-    answer = calculation_function(num2, num2)
+        if input(f"Type 'y' to continue calculating with {answer}, or type 'n' to start a new calculation: ") == "y":
+            num1 = answer
+        else: 
+            should_continue = False
+            calculator()
 
-    calculation_function = operations[operation_symbol]
-    if ask_continue == "n":
 
-print(f"{num2} {operation_symbol} {num2} = {answer}")
+# num1 = int(input("Whats the first number? "))
+# num2 = int(input("What's the next number? "))
+
+# for symbol in operations:
+#     print(symbol)
+
+# operation_symbol = input("Pick an operation from the line above: ")
+# calculation_function = operations[operation_symbol] # important sentence => calculation_function = operations["*"] = multiply
+# ask_continue = input("Type 'y' to continue calculating with 8, or type 'n' to exit.: ")
+# answer = calculation_function(num1, num2) # add(n1, n2), divide(n1, n2)
+
+# end_Calculator = (ask_continue == "y")
+# while end_Calculator:
+#     num2 = int(input("What's the next number? "))
+#     operation_symbol = input("Pick an operation from the line above: ")
+#     answer = calculation_function(num2, num2)
+
+#     calculation_function = operations[operation_symbol]
+#     if ask_continue == "n":
+
+# print(f"{num2} {operation_symbol} {num2} = {answer}")
 
         
 #my calculator code
