781183ac99e6de47a9c01a9d7b9a13ff4655e1fc
Hailey <carly1982@naver.com>
2023-07-08 00:09:57 +0900

    The 18th commit
    Modify guess_number.py (complete the game code.) but just little bit unstable code.

--- day12/guess_number.py
+++ day12/guess_number.py
@@ -1,19 +1,41 @@
 from art import logo
 import random
 
-# Welcome to the Number Guessing Game!
-# I'm thinking of a number between 1 and 100.
-# Choose a difficulty. Type 'easy' or 'hard': 
-# You have 10 attempts remaining to guess the number.
-# Make a guess:
-# Too high. 
-# Too low.
-# Guess again.
-# You got it! The answer was 14.
-# Guess again.
-# You've run out of guesses, you lose.
+print(logo)
+print("Welcome to the Number Guess Game! \nI'm thinking of a number between 1 and 100.")
 
-print("Welcome to the Number Guess Game!")
-print("I'm thinking of a number between 1 and 100.")
+difficulty = input("Choose a difficulty. Type 'easy' or 'hard': ")
 
-difficulty = input(print("Choose a difficulty. Type 'easy' or 'hard': "))
+answer = random.randint(1, 100)
+use_guess = []
+
+def compare():
+    if guess == answer:
+        print("correct.")
+    elif guess > answer:
+        print("Too high.")
+    elif guess < answer:
+        print("Too low.")
+
+if difficulty == "hard":
+    for i in range(0, 5):
+        guess = int(input("Make a guess: "))
+        use_guess.append(guess)
+
+        compare()
+
+    if use_guess[4] != answer:
+        print("You're lose.😭")
+
+elif difficulty == "easy":
+    for i in range(0, 10):
+        guess = int(input("Make a guess: "))
+        use_guess.append(guess)
+        
+        compare()
+    
+    if use_guess[9] != answer:
+        print("You're lose.😂") 
+
+else:
+    print("You don't write 'easy' or 'hard'. Please write again.")
