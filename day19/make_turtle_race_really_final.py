1acd7e0e4a84bc1c8a7f7ea29847759adb1da9aa
Hailey <carly1982@naver.com>
2023-07-28 21:55:10 +0900

    The 50th commit
    Create day24 folder and my_file.txt. (textfile)

--- day19/make_turtle_race_really_final.py
+++ day19/make_turtle_race_really_final.py
@@ -2,22 +2,20 @@ from turtle import Turtle, Screen
 import random
 
 is_race_on = False
-
 screen = Screen()
-screen.setup(width = 500, height = 400)
-user_bet = screen.textinput(title = "Make your bet", prompt = "Which turtle will win the race? Enter a color: ")
+screen.setup(width=500, height=400)
+user_bet = screen.textinput(title="Make your bet", prompt="Which turtle will win the race? Enter a color: ")
 colors = ["red", "orange", "yellow", "green", "blue", "purple"]
 y_positions = [-70, -40, -10, 20, 50, 80]
 all_turtles = []
 
-
+#Create 6 turtles
 for turtle_index in range(0, 6):
-    new_turtle = Turtle(shape = "turtle")
+    new_turtle = Turtle(shape="turtle")
     new_turtle.penup()
     new_turtle.color(colors[turtle_index])
-    new_turtle.goto(x = -230, y = y_positions[turtle_index])
-    all_turtles.append(new_turtle) # we have to multiple turtle instance.
-
+    new_turtle.goto(x=-230, y=y_positions[turtle_index])
+    all_turtles.append(new_turtle)
 
 if user_bet:
     is_race_on = True
@@ -37,14 +35,4 @@ while is_race_on:
         rand_distance = random.randint(0, 10) # include number 10 
         turtle.forward(rand_distance)
 
-
 screen.exitonclick()
-
-
-# timmy = Turtle()
-# tommy = Turtle() 
-
-# 별도의 개체 (서로 다른 인스턴스) 독립적으로 행동이 가능하다. 
-# => 오늘 거북이 경주를 통해 각 인스턴스가 자신의 역할을 할 수 있음을 보여주었다 
-# (독립적으로 작동하고 동작할 수 있는 여러 개체 만들기)
-
