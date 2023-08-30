7d7f9ca8754b4b790b2dbfd6cd74ccc62b57af44
Hailey <carly1982@naver.com>
2023-08-30 23:21:25 +0900

    The 54th Commit
    Create a new squirrels_count.csv file.
    
    df = pandas.DataFrame(data_dict)
    df.to_csv(""day25/squirrels_count.csv")

--- day25/central.py
+++ day25/central.py
@@ -6,4 +6,21 @@ import pandas
 data = pandas.read_csv("day25/2018_Central_Park_Squirrel_Census_-_Squirrel_Data.csv")
 
 gray_squirrels = data[data["Primary Fur Color"] == "Gray"]
-print(len(gray_squirrels))
+
+gray_squirrels_count = len(gray_squirrels)
+cinnamon_squirrels_count = len(data[data["Primary Fur Color"] == "Cinnamon"])
+black_squirrels_count = len(data[data["Primary Fur Color"] == "Black"])
+
+print(gray_squirrels_count)
+print(cinnamon_squirrels_count)
+print(black_squirrels_count)
+
+data_dict = {
+    "Fur Color": ["Gray", "Cinnamon", "Black"],
+    "Count": [gray_squirrels_count, cinnamon_squirrels_count, black_squirrels_count]
+}
+
+print(data_dict)
+
+df = pandas.DataFrame(data_dict) # 데이터프레임으로 초기화시켜주어야 한다
+df.to_csv("day25/squirrels_count.csv")
